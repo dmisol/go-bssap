101cc8b1fc645dcd9aca81c5870f135bcf6340f0
dmisol <dmi3sol@gmail.com>
2025-02-04 14:04:52 +0300

    make it simple

--- pkg/bssmap/bssmap.go
+++ pkg/bssmap/bssmap.go
@@ -2,50 +2,41 @@ package bssmap
 
 import "fmt"
 
-type IE interface {
-	Tag() BssmapIE
-	Encode() []byte
-	Len() uint8
-	String() string
+type IE []byte
+
+func (i IE) Tag() BssmapIE {
+	return BssmapIE(i[0])
 }
 
-type BssmapMsg struct {
+type Bssmap struct {
 	Msg BSSMAP_MsgType
 	IEs []IE
 }
 
-func NewBssmapMsg(mt BSSMAP_MsgType, ie ...IE) *BssmapMsg {
-	m := &BssmapMsg{
+func NewBssmap(mt BSSMAP_MsgType, ies ...IE) *Bssmap {
+	m := &Bssmap{
 		Msg: mt,
-		IEs: ie,
+		IEs: ies,
 	}
 	return m
 }
 
-func (m *BssmapMsg) Encode() []byte {
+func (m *Bssmap) Encode() []byte {
 	r := make([]byte, 1)
 	r[0] = byte(m.Msg)
 
 	for i := range len(m.IEs) {
-		r = append(r, m.IEs[i].Encode()...)
+		r = append(r, m.IEs[i]...)
 	}
 
 	return r
 }
 
-func (m *BssmapMsg) Len() uint8 {
-	l := uint8(1) // MsgType
-	for i := range m.IEs {
-		l += m.IEs[i].Len()
-	}
-	return l
-}
-
-func (m *BssmapMsg) String() string {
+func (m *Bssmap) String() string {
 	s := m.Msg.String() + "{"
 	for i := range len(m.IEs) {
 		if i == 0 {
-			s += m.IEs[i].String()
+			s += m.IEs[i].Tag().String()
 		} else {
 			s += fmt.Sprintf(",%s", m.IEs[i])
 		}
@@ -53,7 +44,7 @@ func (m *BssmapMsg) String() string {
 	return s + "}"
 }
 
-func (m *BssmapMsg) GetIE(tag BssmapIE) (IE, bool) {
+func (m *Bssmap) GetIE(tag BssmapIE) (IE, bool) {
 	for _, v := range m.IEs {
 		if v.Tag() == tag {
 			return v, true
@@ -62,11 +53,11 @@ func (m *BssmapMsg) GetIE(tag BssmapIE) (IE, bool) {
 	return nil, false
 }
 
-func BssmapDecode(b []byte) (*BssmapMsg, error) {
+func BssmapDecode(b []byte) (*Bssmap, error) {
 	if len(b) < 1 {
 		return nil, fmt.Errorf("invalid bssmap message")
 	}
-	m := &BssmapMsg{
+	m := &Bssmap{
 		Msg: BSSMAP_MsgType(b[0]),
 		IEs: make([]IE, 0),
 	}
@@ -84,9 +75,8 @@ func BssmapDecode(b []byte) (*BssmapMsg, error) {
 				return nil, fmt.Errorf("variable IE %q is invalid: [val]", ie)
 			}
 			l := int(b[offset+1])
-			d := &DummyIE{
-				Data: b[offset : offset+l+2],
-			}
+			d := b[offset : offset+l+2]
+
 			m.IEs = append(m.IEs, d)
 			offset += l + 2
 			// Fixed part
@@ -94,9 +84,7 @@ func BssmapDecode(b []byte) (*BssmapMsg, error) {
 			if (offset + ieLen) > len(b) {
 				return nil, fmt.Errorf("fixed IE %q can't fit", ie)
 			}
-			d := &DummyIE{
-				Data: b[offset : offset+ieLen],
-			}
+			d := b[offset : offset+ieLen]
 			m.IEs = append(m.IEs, d)
 			offset += ieLen
 			// Unsupported IE
