8d42205a2ff540607270d958999d21bcad57af47
Ilya Serov <isserov@hex.team>
2025-09-16 18:09:53 +0300

    add bssap part for ASSCOMP/ASSFAIL in dirty.go

--- pkg/dtap/dirty.go
+++ pkg/dtap/dirty.go
@@ -1,6 +1,7 @@
 package dtap
 
 func AssResult(msg Msg_Type, c CAUSE) []byte {
-	b := []byte{byte(PD_RR), byte(msg), byte(c)}
+	// TODO: fix second byte
+	b := []byte{0x01, 0x00, 0x03, byte(PD_RR), byte(msg), byte(c)}
 	return b
 }
